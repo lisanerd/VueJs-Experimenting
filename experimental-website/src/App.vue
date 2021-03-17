175289d0042a34c2a542009378041c2c3e16a189
lisanerd <lisa.rm.nyc@gmail.com>
2021-03-17 16:27:57 -0400

    Resized image

--- experimental-website/src/App.vue
+++ experimental-website/src/App.vue
@@ -1,19 +1,21 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/MathVueJsWebsite.jpeg">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/MathVueJsWebsite.jpeg" 
+    height="200px" width="300px"
+    />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+};
 </script>
 
 <style>
