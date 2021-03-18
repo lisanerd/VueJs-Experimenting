a0f0206c89071f2c63d925835d11992394f268cd
lisanerd <lisa.rm.nyc@gmail.com>
2021-03-17 21:22:20 -0400

    Added hide and show buttons. Trying to make text show and hide based on when I click a button.

--- experimental-website/src/App.vue
+++ experimental-website/src/App.vue
@@ -6,8 +6,14 @@
       height="200px"
       width="300px"
     />
-    <ParticleBtn :options="btnOps" cls="btn-cls"> hello eveybody! </ParticleBtn>
+    <ParticleBtn :options="btnOps" cls="btn-cls"> Cklick me! </ParticleBtn>
     <button @click="btnOps.visible = !btnOps.visible">toggle</button>
+    <ParticleBtn v-on:click="isHidden = true">Hide the text below</ParticleBtn>
+    <ParticleBtn v-on:click="isHidden = !isHidden">
+      Toggle hide and show
+    </ParticleBtn>
+
+    <h1 v-if="!isHidden">Hide me on click event!</h1>
   </div>
 </template>
 
@@ -40,6 +46,14 @@ export default {
         },
       },
     };
+  },
+  mounted() {
+    let recaptchaScript = document.createElement("script");
+    recaptchaScript.setAttribute(
+      "src",
+      "https://cdn.jsdelivr.net/npm/vue@2.5.13/dist/vue.js"
+    );
+    document.head.appendChild(recaptchaScript);
   },
 };
 </script>
